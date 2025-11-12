33241532a77046f968a9f94b1ba953287d45ec55
LiuSiChuang <160833587+LiuSiChuang@users.noreply.github.com>
2025-11-13 01:54:51 +0700

    Update settings.py

--- blogicum/settings.py
+++ blogicum/settings.py
@@ -1,4 +1,4 @@
-import os
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,5 +79,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
